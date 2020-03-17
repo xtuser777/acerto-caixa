6d1a71f7c0d0d99542d87515c953f9bf0bc68f5c
lucaoxt@unoeste.edu.br <lucaoxt@unoeste.edu.br>
2020-03-17 09:02:49 -0300

    Correção de models e dao, aplicação correta do singleton na classe conexão e desenvolvimento da camada de controle.

--- src/br/unoeste/fipp/testecaixa/control/AcertoCaixaControl.java
+++ src/br/unoeste/fipp/testecaixa/control/AcertoCaixaControl.java
@@ -1,6 +1,23 @@
 package br.unoeste.fipp.testecaixa.control;
 
+import br.unoeste.fipp.testecaixa.dao.Conexao;
+import br.unoeste.fipp.testecaixa.model.Caixa;
+import java.sql.Connection;
+
 public class AcertoCaixaControl 
 {
-    
+    public String confirmar(int tipo, double valor, String motivo, int caixaId){
+        String erro = "";
+        Caixa caixa = Caixa.getById(caixaId);
+        
+        Connection conn  = Conexao.open();
+        
+        if(caixa.isStatus()){
+            
+            
+        }
+        else
+            return "Caixa fechado.";
+        return erro;
+    }
 }
